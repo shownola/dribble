1192506d2a9d489231351f734e8700f78828499e
SHERRY <swasieko@gmail.com>
2018-02-05 18:09:11 +0000

    Add comments to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
+  has_many :comments
 end
